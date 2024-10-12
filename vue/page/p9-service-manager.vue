45cc81bb245826e8ae2294eb72b07f562583dfbe
nhj12311 <nhj12311@gmail.com>
2024-10-12 14:13:35 +0900

    feat : 토스 비교금리 추가.

--- vue/page/p9-service-manager.vue
+++ vue/page/p9-service-manager.vue
@@ -3,18 +3,10 @@
     <v-row>
       <v-col cols="12">
         <v-card>
-          
+
           <v-card-text>
-            <v-treeview
-              :items="treeItems"
-              activatable="true"
-              item-value="id"
-              item-key="id"
-              open-all              
-              return-object
-              class="elevation-1"
-              dense              
-            >
+            <v-treeview :items="treeItems" activatable="true" item-value="id" item-key="id" return-object
+              class="elevation-1" dense>
               <template v-slot:prepend="{ item, open }">
                 <v-icon v-if="item.children && item.children.length > 0">
                   {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
@@ -29,7 +21,8 @@
               <template v-slot:label="{ item }">
                 <div class="d-flex justify-space-between align-center">
                   <div>
-                    <strong>{{ item.serviceName || item.txName }}</strong> ({{ item.serviceName ? item.serviceID : item.txID }})
+                    <strong>{{ item.serviceName || item.txName }}</strong> ({{ item.serviceName ? item.serviceID :
+                    item.txID }})
                   </div>
                   <!--div>
                     <v-btn icon small @click.stop="editService(item)">
@@ -60,24 +53,12 @@
         </v-card-title>
         <v-card-text>
           <v-form ref="form" v-model="isFormValid">
-            <v-text-field
-              v-model="form.serviceID"
-              label="서비스 ID"
-              :rules="[v => !!v || '서비스 ID는 필수입니다.']"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.serviceName"
-              label="서비스명"
-              :rules="[v => !!v || '서비스명은 필수입니다.']"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.description"
-              label="설명"
-              :rules="[v => !!v || '설명은 필수입니다.']"
-              required
-            ></v-text-field>
+            <v-text-field v-model="form.serviceID" label="서비스 ID" :rules="[v => !!v || '서비스 ID는 필수입니다.']"
+              required></v-text-field>
+            <v-text-field v-model="form.serviceName" label="서비스명" :rules="[v => !!v || '서비스명은 필수입니다.']"
+              required></v-text-field>
+            <v-text-field v-model="form.description" label="설명" :rules="[v => !!v || '설명은 필수입니다.']"
+              required></v-text-field>
           </v-form>
         </v-card-text>
         <v-card-actions>
@@ -113,104 +94,131 @@ module.exports = {
       ],
       services: [
         // 초기 서비스 데이터 예시
+
         {
-          id : 1,
+          id: 1,
           serviceID: 'SV001',
           serviceName: '여신 한도조회',
           description: '사용자의 여신 한도를 조회하는 서비스입니다.',
           transactions: [
             {
-              id : 11,
+              id: 11,
               txID: 'IF011',
               txName: 'NICE 신용조회',
-              status: '성공',              
+              status: '성공',
             },
             {
-              id : 12,
+              id: 12,
               txID: 'IF012',
               txName: 'KCB 신용조회',
               status: '성공'
             },
             {
-              id : 13,
+              id: 13,
               txID: 'IF013',
               txName: 'CSS 한도/금리 조회',
-              status: '성공',              
+              status: '성공',
             },
           ]
         },
-        
         {
-          id : 2,
+          id: 5,
+          serviceID: 'SV005',
+          serviceName: '토스 비교금리조회',
+          description: '토스 비교금리를 조회하는 서비스입니다.',
+          transactions: [
+            {
+              id: 11,
+              txID: 'IF011',
+              txName: 'NICE 신용조회',
+              status: '성공',
+            },
+            {
+              id: 12,
+              txID: 'IF012',
+              txName: 'KCB 신용조회',
+              status: '성공'
+            },
+            {
+              id: 13,
+              txID: 'IF013',
+              txName: 'CSS 한도/금리 조회',
+              status: '성공',
+            },
+          ]
+        },
+
+        {
+          id: 2,
           serviceID: 'SV002',
           serviceName: '모바일 뱅킹 로그인',
           description: '모바일 뱅킹 로그인 서비스입니다.',
           transactions: [
             {
-              id : 20,
+              id: 20,
               txID: 'IF020',
               txName: '앱버전 체크(안드로이드/아이폰)',
               status: '성공'
             },
             {
-              id : 21,
+              id: 21,
               txID: 'IF021',
               txName: 'FSB(중앙회) Open API 거래',
               status: '성공'
             },
             {
-              id : 22,
+              id: 22,
               txID: 'IF022',
               txName: 'FDS 체크',
               status: '성공'
             },
             {
-              id : 23,
+              id: 23,
               txID: 'IF023',
               txName: '계정계 로그인 조회',
               status: '성공'
             },
             {
-              id : 24,
+              id: 24,
               txID: 'IF024',
               txName: '계정계2 로그인 조회',
               status: '성공'
-            },            
+            },
           ]
         },
 
         {
-          id : 3,
+          id: 3,
           serviceID: 'SV003',
           serviceName: '햇살론 대출실행',
           description: '여신 햇살론 상품 대출실행 서비스입니다.',
           transactions: [
             {
-              id : 31,
+              id: 31,
               txID: 'IF031',
               txName: 'NICE 실시간대출조회',
-              status: '성공',              
+              status: '성공',
             },
             {
-              id : 32,
+              id: 32,
               txID: 'IF032',
               txName: '서민금융진흥원 보증실행',
               status: '성공'
             },
             {
-              id : 33,
+              id: 33,
               txID: 'IF033',
               txName: 'FSB(중앙회) 대출실행',
-              status: '성공',              
+              status: '성공',
             },
             {
-              id : 34,
+              id: 34,
               txID: 'IF034',
               txName: '서민금융진흥원 대출통지',
               status: '성공'
             },
             {
-              id : 35,
+              id: 35,
               txID: 'IF035',
               txName: '대출실행 회계 전표 처리',
               status: '성공',
@@ -219,33 +227,33 @@ module.exports = {
         },
 
 
-
+        
         {
-          id : 4,
+          id: 4,
           serviceID: 'SV004',
           serviceName: '여신 가상계좌 원리금 수납',
           description: '여신 상품 가상계좌 원리금 수납 서비스입니다.',
           transactions: [
             {
-              id : 41,
+              id: 41,
               txID: 'IF041',
               txName: 'KIBNET 가상계좌 입금',
-              status: '성공',              
+              status: '성공',
             },
             {
-              id : 42,
+              id: 42,
               txID: 'IF042',
               txName: 'FSB(중앙회) 원리금 수납',
               status: '성공'
             },
             {
-              id : 43,
+              id: 43,
               txID: 'IF043',
               txName: 'FSB(중앙회) 원리금 수납',
-              status: '성공',              
-            },            
+              status: '성공',
+            },
             {
-              id : 45,
+              id: 45,
               txID: 'IF045',
               txName: '회계 전표 처리',
               status: '성공',
